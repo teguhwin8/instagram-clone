db5e4f478fbb18979aeb2f38efa493962b3d4f45
Mr. T <43848500+teguhwin8@users.noreply.github.com>
2021-10-16 20:37:22 +0700

    build stories

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,18 @@
 import Head from 'next/head'
+import Feed from "../components/Feed"
 import Header from "../components/Header"
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-gray-50 h-screen overflow-y-scroll scrollbar-hide">
       <Head>
         <title>Instagram Clone by teguhwin8</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
+
+      <Feed />
 
     </div>
   )
